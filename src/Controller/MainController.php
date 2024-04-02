a21f085ec09cd3a284d539ac01f69b86f43f901a
lohengrin <oljn22@student.bth.se>
2024-04-02 12:47:08 +0200

    style lucky etc

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -28,8 +28,55 @@ class MainController extends AbstractController
         $this->data = [
             "siteTitle" => "MVC",
             "pageTitle" => "",
+            "luckyNum" => null,
+            "luckyCredits" => ""
         ];
     }
+
+
+
+    /**
+     * Manipulate old session value, and set data["luckyNum"]
+     *
+     * @return void
+     */
+    private function updateLuckyNum()
+    {
+        session_name("oljn22");
+        session_start();
+
+        if (!isset($_SESSION["lucky_num"])) {
+            // set random int 0-4
+            $_SESSION["lucky_num"] = random_int(0, 4);
+        } else {
+            // continue loop 0-4
+            $_SESSION["lucky_num"] = ($_SESSION["lucky_num"] + 1) % 5;
+        }
+
+        $this->data["luckyNum"] = $_SESSION["lucky_num"];
+    }
+
+
+
+    /**
+     * get credits for current lucky image from relevant folder,
+     * and update data["luckyCredits"]
+     * 
+     * @return void
+     */
+    private function updateLuckyCredits()
+    {
+        $num = $this->data["luckyNum"];
+        $filePath = "img/lucky/{$num}/credits.txt";
+        $fileContent = "";
+
+        if (file_exists($filePath)) {
+            $fileContent = file_get_contents($filePath);
+        }
+
+        $this->data["luckyCredits"] = $fileContent;
+    }
+
 
 
 
@@ -59,5 +106,19 @@ class MainController extends AbstractController
         $this->data["pageTitle"] = "Redovisning";
 
         return $this->render("report.html.twig", $this->data);
+    }
+
+
+
+    #[Route('/lucky', name: "lucky")]
+    public function lucky()
+    {
+        $this->data["pageTitle"] = "Lyckobild";
+
+        // update luckyNum and luckyCredits to render an img with credits
+        $this->updateLuckyNum();
+        $this->updateLuckyCredits();
+
+        return $this->render("lucky.html.twig", $this->data);
     }
 }
