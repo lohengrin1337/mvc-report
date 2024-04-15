3b5a9d1276e7fb587e2607e0a14e882b22d44c9a
lohengrin <oljn22@student.bth.se>
2024-04-15 14:55:34 +0200

    Add landingpage for card excercise
    Add route /card - CardConroller::start().
    Add template/card/start.

--- src/Controller/CardController.php
+++ src/Controller/CardController.php
@@ -8,7 +8,38 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
+
+
+/**
+ * Controller class with routes for card assignment
+ */
 class CardController extends AbstractController
 {
+    /**
+     * @var array $data - template data
+     */
+    private $data;
 
+
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->data = [
+            "siteTitle" => "MVC",
+            "pageTitle" => "",
+        ];
+    }
+
+
+
+    #[Route("/card", name: "card_start", methods: ["GET"])]
+    public function start(): Response
+    {
+        $this->data["pageTitle"] = "Kortlek";
+
+        return $this->render("card/start.html.twig", $this->data);
+    }
 }
