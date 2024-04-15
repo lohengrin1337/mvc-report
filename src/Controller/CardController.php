ef4541c1f9d7a885c40005d97765cf276d6f96bc
lohengrin <oljn22@student.bth.se>
2024-04-15 15:07:15 +0200

    Add new card routes
    '/card/deck', '/card/deck/shuffle', '/card/deck/draw' with templates

--- src/Controller/CardController.php
+++ src/Controller/CardController.php
@@ -30,6 +30,7 @@ class CardController extends AbstractController
         $this->data = [
             "siteTitle" => "MVC",
             "pageTitle" => "",
+            "deck" => null,
         ];
     }
 
@@ -41,5 +42,35 @@ class CardController extends AbstractController
         $this->data["pageTitle"] = "Kortlek";
 
         return $this->render("card/start.html.twig", $this->data);
+    }
+
+
+
+    #[Route("/card/deck", name: "card_deck", methods: ["GET"])]
+    public function deck(): Response
+    {
+        $this->data["pageTitle"] = "Visa Kortlek";
+
+        return $this->render("card/deck.html.twig", $this->data);
+    }
+
+
+
+    #[Route("/card/deck/shuffle", name: "card_deck_shuffle", methods: ["GET"])]
+    public function deckShuffle(): Response
+    {
+        $this->data["pageTitle"] = "Visa blandad Kortlek";
+
+        return $this->render("card/deck_shuffle.html.twig", $this->data);
+    }
+
+
+
+    #[Route("/card/deck/draw", name: "card_deck_draw", methods: ["GET"])]
+    public function deckDraw(): Response
+    {
+        $this->data["pageTitle"] = "Dra kort";
+
+        return $this->render("card/deck_draw.html.twig", $this->data);
     }
 }
