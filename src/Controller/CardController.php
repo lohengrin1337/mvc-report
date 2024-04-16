22574507d52bf6788b3e17a5c617019095def5e4
lohengrin <oljn22@student.bth.se>
2024-04-16 08:29:24 +0200

    Add logic to routes
    Handle CardDeck and session in different routes

--- src/Controller/CardController.php
+++ src/Controller/CardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Card\CardDeck;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -30,16 +31,21 @@ class CardController extends AbstractController
         $this->data = [
             "siteTitle" => "MVC",
             "pageTitle" => "",
-            "deck" => null,
+            "cardDraw" => [],
+            "allCards" => [],
+            "deckCount" => [],
         ];
     }
 
 
 
     #[Route("/card", name: "card_start", methods: ["GET"])]
-    public function start(): Response
+    public function start(SessionInterface $session): Response
     {
         $this->data["pageTitle"] = "Kortlek";
+
+        // init a new deck of cards, and save to session
+        $session["card_deck"] = new CardDeck();
 
         return $this->render("card/start.html.twig", $this->data);
     }
@@ -47,9 +53,13 @@ class CardController extends AbstractController
 
 
     #[Route("/card/deck", name: "card_deck", methods: ["GET"])]
-    public function deck(): Response
+    public function deck(SessionInterface $session): Response
     {
         $this->data["pageTitle"] = "Visa Kortlek";
+
+        $deck = $session["card_deck"];
+
+        $this->data["allCards"] = $deck->getAsString();
 
         return $this->render("card/deck.html.twig", $this->data);
     }
@@ -57,9 +67,14 @@ class CardController extends AbstractController
 
 
     #[Route("/card/deck/shuffle", name: "card_deck_shuffle", methods: ["GET"])]
-    public function deckShuffle(): Response
+    public function deckShuffle(SessionInterface $session): Response
     {
         $this->data["pageTitle"] = "Visa blandad Kortlek";
+
+        $deck = $session["card_deck"];
+        $deck->shuffle();
+
+        $this->data["allCards"] = $deck->getAsString();
 
         return $this->render("card/deck_shuffle.html.twig", $this->data);
     }
@@ -67,9 +82,16 @@ class CardController extends AbstractController
 
 
     #[Route("/card/deck/draw", name: "card_deck_draw", methods: ["GET"])]
-    public function deckDraw(): Response
+    public function deckDraw(SessionInterface $session): Response
     {
         $this->data["pageTitle"] = "Dra kort";
+
+        $deck = $session["card_deck"];
+
+        $this->data["cardDraw"] = $deck->draw();
+        $this->data["deckCount"] = $deck->getCount();
+
+        $session["card_deck"] = $deck;
 
         return $this->render("card/deck_draw.html.twig", $this->data);
     }
