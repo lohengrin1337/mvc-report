c0a25cfdaf654e392afaf0ba72d93bda5c8dbe72
lohengrin <oljn22@student.bth.se>
2024-04-18 12:24:33 +0200

    Work on api for kmom02 assignment
    several api routes
    templates

--- src/Controller/CardApiController.php
+++ src/Controller/CardApiController.php
@@ -6,6 +6,7 @@ use App\Card\CardDeck;
 use App\Card\CardHand;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -70,4 +71,101 @@ class CardApiController extends AbstractController
 
         return $this->response;
     }
+
+
+
+    #[Route("/api/deck/draw", name: "api_deck_draw", methods: ["POST"])]
+    public function apiDeckDraw(
+        Request $request,
+        SessionInterface $session
+        ): JsonResponse
+    {
+        $number = $request->request->get("number") ?? 1;
+        // $number = $request->request->get("number") ?? null;
+        // if (!$number) {
+        //     throw new \Exception("Antal kort saknades i request (POST) '/api/deck/draw'");
+        // }
+
+        $deck = $session->get("deck") ?? null;
+        if (!$deck) {
+            $deck = new CardDeck;
+        }
+
+        $hand = new CardHand();
+        $hand->draw($deck, $number);
+        $session->set("deck", $deck);
+
+        $data = [
+            "hand" => $hand->getAsString(),
+            "deckCount" => $deck->getCount(),
+        ];
+
+        $this->setResponse($data);
+
+        return $this->response;
+    }
+
+
+
+    #[Route("/api/deck/deal", name: "api_deck_deal", methods: ["POST"])]
+    public function apiDeckDeal(
+        Request $request,
+        SessionInterface $session
+        ): JsonResponse
+    {
+        $players = $request->request->get("players") ?? null;
+        if (!$players) {
+            throw new \Exception("Antal spelare saknades i request (POST) '/api/deck/deal'");
+        }
+        $cards = $request->request->get("cards") ?? null;
+        if (!$cards) {
+            throw new \Exception("Antal kort saknades i request (POST) '/api/deck/deal'");
+        }
+        $deck = $session->get("deck") ?? null;
+        if (!$deck) {
+            $deck = new CardDeck;
+        }
+
+        $data = [];
+        for ($i = 1; $i <= $players; $i++) {
+            $hand = new CardHand();
+            $hand->draw($deck, $cards);
+            $data["Player {$i}"] = $hand->getAsString();
+        }
+        $data["deckCount"] = $deck->getCount();
+        $session->set("deck", $deck);
+
+        $this->setResponse($data);
+
+        return $this->response;
+    }
+
+
+
+    // #[Route("/api/deck/draw/{number<\d+>}", name: "api_deck_draw_num", methods: ["POST"])]
+    // public function apiDeckDrawNum(
+    //     int $number,
+    //     SessionInterface $session
+    //     ): JsonResponse
+    // {
+    //     $deck = $session->get("deck") ?? null;
+    //     if (!$deck) {
+    //         $deck = new CardDeck;
+    //     }
+
+    //     $hand = new CardHand();
+    //     $hand->draw($deck, $number);
+    //     $session->set("deck", $deck);
+
+    //     $data = [
+    //         "hand" => $hand->getAsString(),
+    //         "deckCount" => $deck->getCount(),
+    //     ];
+
+    //     $this->setResponse($data);
+
+    //     return $this->response;
+    // }
 }
+
+// json_encode($deck)
