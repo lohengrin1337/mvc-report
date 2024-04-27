61b0cd1cced5a85c942c752eb394b8a4eb451b10
lohengrin <oljn22@student.bth.se>
2024-04-27 22:09:08 +0200

    Add CardDeckTest and run suite with coverage

---
+++ tests/Card/CardDeckTest.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Card;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Test cases for class CardDeck.
+ */
+class CardDeckTest extends TestCase
+{
+    protected CardDeck $deck;
+
+    protected function setUp(): void
+    {
+        // deck of CardGraphic cards
+        $this->deck = new CardDeck(CardGraphic::class);
+
+        // $this->deck = new CardDeck(
+        //     $this->createStub(CardGraphic::class)::class
+        // );
+    }
+
+
+
+    /**
+     * Construct object and verify that the object has the expected
+     * properties.
+     */
+    public function testCreateCardDeck()
+    {
+        $this->assertInstanceOf(CardDeck::class, $this->deck);
+        $this->assertIsString($this->deck->getCardBack());
+        $this->assertEquals(52, $this->deck->getCount());
+    }
+
+
+
+    /**
+     * Construct object with invalid arg, expect exception
+     */
+    public function testCreateCardDeckInvalid()
+    {
+        $this->expectException("InvalidArgumentException");
+        $deck = new CardDeck("NotACardInterface::class");
+    }
+
+
+
+    /**
+     * Get cards as string[], and assert count 52, and 
+     */
+    public function testGetAsString()
+    {
+        $res = $this->deck->getAsString();
+        $this->assertCount(52, $res);
+        $this->assertContainsOnly ("string", $res);
+    }
+
+
+
+    /**
+     * Sorted deck is not equal to shuffled deck,
+     * and two shuffled decks are not equal,
+     * but two sorted decks are equal
+     */
+    public function testSortAndShuffle()
+    {
+        $this->deck->shuffle();
+        $shuffled1 = $this->deck->getAsString();
+        $this->deck->sort();
+        $sorted1 = $this->deck->getAsString();
+        $this->deck->shuffle();
+        $shuffled2 = $this->deck->getAsString();
+        $this->deck->sort();
+        $sorted2 = $this->deck->getAsString();
+
+        $this->assertNotEquals($shuffled1, $sorted1);
+        $this->assertNotEquals($shuffled1, $shuffled2);
+        $this->assertEquals($sorted1, $sorted2);
+    }
+
+
+
+    /**
+     * Sort deck, and draw. assert card is 13 of clubs
+     */
+    public function testDrawWhenSorted()
+    {
+        $this->deck->sort();
+        $res = $this->deck->draw();
+        $this->assertEquals("clubs", $res->getSuit());
+        $this->assertEquals(13, $res->getRank());
+    }
+}
