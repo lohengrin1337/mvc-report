73c4ab757063e5f39c1d65330ecd02c2044cb76b
lohengrin <oljn22@student.bth.se>
2024-04-27 16:22:15 +0200

    Add CardGraphicTest and run test plus coverage

---
+++ tests/Card/CardGraphicTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Card;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Test cases for class CardGraphic.
+ */
+class CardGraphicTest extends TestCase
+{
+    protected CardGraphic $card;
+
+    protected function setUp(): void
+    {
+        $this->card = new CardGraphic("diamonds", 3); // 3 of diamonds
+    }
+
+
+
+    /**
+     * Get cardback and assert correct
+     */
+    public function testCorrectCardBack()
+    {
+        $res = $this->card->getCardBack();
+        $this->assertEquals("&#x1f0a0", $res);
+    }
+
+
+
+    /**
+     * Get string repr. of reg. rank (3) and assert correct
+     */
+    public function testCorrectStringDigit()
+    {
+        $res = $this->card->getAsString();
+        $this->assertEquals("&#x1f0c3", $res);
+    }
+
+
+
+    /**
+     * Get string repr. face card and assert correct
+     */
+    public function testCorrectStringFace()
+    {
+        $this->card->setRank(14); // ace
+        $res = $this->card->getAsString();
+        $this->assertEquals("&#x1f0c1", $res);
+    }
+}
