dbedb8535f9b95fadaac24d053790a1a03505225
lohengrin <oljn22@student.bth.se>
2024-05-26 13:25:10 +0200

    Write trait for rules with multiple of a kind

---
+++ src/PokerSquares/OfAKindTrait.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\PokerSquares;
+
+trait OfAKindTrait
+{
+    /**
+     * @var int $amount - the amount of a kind for the rule
+     *                    is set in the constructor of the rule class
+     */
+    private int $amount;
+
+
+
+    /**
+     * @param CardInterface[] $cards - array of cards to check
+     * @return bool - true if rule is met
+     */
+    public function checkHand(array $cards): bool
+    {
+        // get the rank of all cards
+        $ranks = array_map(
+            function($card)
+            {
+                return $card->getRank();
+            },
+            $cards
+        );
+
+        // get count of unique ranks (rank => count)
+        $uniqueRankCount = array_count_values($ranks);
+
+        // check condition of rule
+        $ruleIsMet = false;
+        foreach ($uniqueRankCount as $count) {
+            if ($count >= $this->amount) {
+                $ruleIsMet = true;
+            } 
+        }
+
+        return $ruleIsMet;
+    }
+}
