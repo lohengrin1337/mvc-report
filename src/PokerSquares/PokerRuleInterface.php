c75b31ece12f138231e3666b098e666e9b57993a
lohengrin <oljn22@student.bth.se>
2024-05-26 13:24:12 +0200

    Write interface for poker rules

---
+++ src/PokerSquares/PokerRuleInterface.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\PokerSquares;
+
+use App\Card\CardInterface;
+
+/**
+ * Interface for poker hand rules
+ */
+interface PokerRuleInterface
+{
+    /**
+     * Check if a hand meets the rules of a poker hand
+     * 
+     * @param CardInterface[] $cards - array of cards to check
+     * @return bool - true if rule is met
+     */
+    public function checkHand(array $cards): bool;
+
+
+
+    /**
+     * Get name of rule
+     * 
+     * @return string - name of rule
+     */
+    public function getName(): string;
+}
