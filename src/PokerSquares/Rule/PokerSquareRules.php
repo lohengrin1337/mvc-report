91137ce815e3801ef8efaa50fd19ef92d34b2895
lohengrin <oljn22@student.bth.se>
2024-05-27 20:05:13 +0200

    Add a collection class for rules

---
+++ src/PokerSquares/Rule/PokerSquareRules.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\PokerSquares\Rule;
+
+/**
+ * Class holding a set of Poker Square rules
+ */
+class PokerSquareRules implements RuleCollectionInterface
+{
+    /**
+     * @var PokerRuleInterface[] $rules - a set of rules
+     */
+    private array $rules;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->rules = [
+            new OnePair(),
+            new HighCard(),
+        ];
+    }
+
+
+    /**
+     * Use rules to assess highest met rule, or none
+     *
+     * @param CardInterface[] $hand
+     * @return string - rule name of best matching rule
+     */
+    public function assessHand(array $hand): string
+    {
+        foreach ($this->rules as $rule) {
+            if ($rule->checkHand($hand)) {
+                return $rule->getName();
+            }
+
+            return "";  // occurs if hand is empty
+        }
+    }
+}
