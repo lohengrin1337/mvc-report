63f4d07e22d0a8edcb530b7200b3061f102b33b4
lohengrin <oljn22@student.bth.se>
2024-05-27 20:04:48 +0200

    Add rule HighCard

---
+++ src/PokerSquares/Rule/HighCard.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\PokerSquares\Rule;
+
+use App\Trait\Nameable;
+
+/**
+ * Class for one pair rule - poker hand
+ */
+class HighCard implements PokerRuleInterface
+{
+    use Nameable;
+    use OfAKindTrait;
+
+    /**
+     * Constructor
+     * Set name of rule
+     * Set amount to 1 (of a kind)
+     */
+    public function __construct()
+    {
+        $this->setName("high-card");
+        $this->amount = 1;
+    }
+}
