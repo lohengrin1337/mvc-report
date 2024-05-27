7775606648ef660812e42ba2d9902acf65f1a27b
lohengrin <oljn22@student.bth.se>
2024-05-27 20:05:57 +0200

    Implement new classes in game class

--- src/PokerSquares/PokerSquaresGame.php
+++ src/PokerSquares/PokerSquaresGame.php
@@ -4,13 +4,27 @@ namespace App\PokerSquares;
 
 use App\Card\CardDeck;
 use App\PokerSquares\Rule\PokerRuleInterface;
+use App\PokerSquares\Rule\RuleCollectionInterface;
 
+/**
+ * Game engine for Poker Squares
+ */
 class PokerSquaresGame
 {
     /**
-     * @var PokerRuleInterface[] $rules - set of rules
+     * @var RuleCollectionInterface $rules - set of rules
      */
-    private array $rules;
+    private RuleCollectionInterface $rules;
+
+    /**
+     * @var ScoreMappingInterface $scoreMapper - maps a rule to a score
+     */
+    private ScoreMappingInterface $scoreMapper;
+
+    /**
+     * @var Score $score
+     */
+    private Score $score;
 
     /**
      * @var GameBoard $gameboard
@@ -29,20 +43,26 @@ class PokerSquaresGame
 
     /**
      * Constructor
-     * Set rules, gamebord, player and deck
+     * Set rules, score mapper, gamebord, player and deck
      * 
-     * @param PokerRuleInterface[] $rules - set of rules
+     * @param RuleCollectionInterface $rules - set of rules
+     * @param ScoreMappingInterface $scoreMapper
+     * @param Score $score
      * @param GameBoard $gameboard
      * @param Player $player
      * @param CardDeck $deck
      */
     public function __construct(
-        array $rules,
+        RuleCollectionInterface $rules,
+        ScoreMappingInterface $scoreMapper,
+        Score $score,
         GameBoard $gameboard,
         Player $player,
         CardDeck $deck
     ){
         $this->$rules = $rules;
+        $this->$scoreMapper = $scoreMapper;
+        $this->$score = $score;
         $this->$gameboard = $gameboard;
         $this->$player = $player;
         $this->$deck = $deck;
