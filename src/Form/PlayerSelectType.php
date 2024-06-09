77e491841f85a9f444bc45dec2b4e1543a249991
lohengrin <oljn22@student.bth.se>
2024-06-09 20:09:57 +0200

    Create new form types, add validation and style

---
+++ src/Form/PlayerSelectType.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Player;
+use App\Repository\PlayerRepository;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PlayerSelectType extends AbstractType
+{
+    private PlayerRepository $playerRepository;
+
+    /**
+     * Constructor
+     */
+    public function __construct(PlayerRepository $playerRepository)
+    {
+        $this->playerRepository = $playerRepository;
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $players = $this->playerRepository->getAllSortedByName();
+
+        $builder
+            ->add(
+                'player',
+                ChoiceType::class,
+                [
+                    'label' => false,
+                    'placeholder' => 'Välj en spelare',
+                    'choices' => $players,
+                    'choice_value' => 'name',
+                    'choice_label' => function (Player $player): string {
+                        return $player->getName();
+                    },
+                ]
+            )
+            ->add(
+                'save',
+                SubmitType::class,
+                [
+                    'label' => $options['submit_label'],
+                    'attr' => ['class' => 'button']
+                ]
+            )
+        ;
+    }
+
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'attr' => ['class' => 'form'],
+            'submit_label' => 'Välj',
+        ]);
+    }
+}
