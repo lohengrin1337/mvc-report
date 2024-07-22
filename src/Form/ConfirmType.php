96fe49f6fa8127446a57c74abcc83d0eb8b3e21c
lohengrin <oljn22@student.bth.se>
2024-07-22 16:25:33 +0200

    Add confirmType for regular confirm button form

---
+++ src/Form/ConfirmType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ConfirmType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder->add(
+            "confirm",
+            SubmitType::class,
+            [
+                "label" => "Starta spelet",
+                "attr" => ["class" => "button"]
+            ]
+        );
+    }
+
+    // public function configureOptions(OptionsResolver $resolver): void
+    // {
+    //     $resolver->setDefaults([
+    //         "attr" => ["class" => "form"],
+    //     ]);
+    // }
+}
